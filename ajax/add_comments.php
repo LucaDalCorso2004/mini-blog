13d2c58943ac9f9df69d2724c3427a0b5c3c4761
LucaDalCorso2004 <L.DalCorso.inf21@stud.bbbaden.ch>
2025-07-09 16:24:00 +0200

    create and view comments

---
+++ ajax/add_comments.php
@@ -0,0 +1,42 @@
+ <?php
+require_once '../includes/db.php';// PDO-Verbindung
+
+
+ function create($title, $content)
+    {
+ global $conn;       
+        $date = date("Y/m/d H:i");
+        var_dump($title);
+
+        try {
+            $sql = "INSERT INTO posts (title, content, created_at)
+                    VALUES (:title, :content, :created_at)";
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute([
+                ':title' => $title,
+                ':content' => $content,
+                ':created_at' => $date,
+                
+            ]);
+            return $result;
+        } catch (PDOException $e) {
+            
+            return false;
+        }
+    }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'] ?? '';
+    $content = $_POST['content'] ?? '';
+
+    if ($title && $content) {
+        $success = create( $title, $content);
+        if ($success) {
+            echo "Post erfolgreich erstellt.";
+        } else {
+            echo "Fehler beim Erstellen des Posts.";
+        }
+    } else {
+        echo "Bitte alle Felder ausfüllen.";
+    }
+}
+?>
---
+++ post.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="ajax/add_comments.php" method="post">
+
+  Titel: <input type="text" name="title" required><br>
+    content: <input type="text" name="content" required><br>
+    <button type="submit">Post</button>
+</form>
+
+</body>
+</html>
